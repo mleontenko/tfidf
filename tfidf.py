b896e548eff57567efbc081b30455917e7fd79f7
Marin Leontenko <marin.leontenko@gmail.com>
2021-07-31 09:53:15 +0200

    dictionary of words and occurence for each document

--- tfidf.py
+++ tfidf.py
@@ -1,2 +1,17 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+documentA = 'the man went out for a walk'
+documentB = 'the children sat around the fire'
+
+bagOfWordsA = documentA.split(' ')
+bagOfWordsB = documentB.split(' ')
+
+uniqueWords = set(bagOfWordsA).union(set(bagOfWordsB))
+
+numOfWordsA = dict.fromkeys(uniqueWords, 0)
+for word in bagOfWordsA:
+    numOfWordsA[word] += 1
+numOfWordsB = dict.fromkeys(uniqueWords, 0)
+for word in bagOfWordsB:
+    numOfWordsB[word] += 1
