5c90be467e03446e52202852204f8a957c882ff7
Marin Leontenko <marin.leontenko@gmail.com>
2021-08-01 11:05:47 +0200

    compute IDF

--- tfidf.py
+++ tfidf.py
@@ -26,5 +26,20 @@ def computeTF(wordDict, bagOfWords):
 tfA = computeTF(numOfWordsA, bagOfWordsA)
 tfB = computeTF(numOfWordsB, bagOfWordsB)
 
-print(tfA)
-print(tfB)
+def computeIDF(documents):
+    import math
+    N = len(documents)
+    
+    idfDict = dict.fromkeys(documents[0].keys(), 0)
+    for document in documents:
+        for word, val in document.items():
+            if val > 0:
+                idfDict[word] += 1
+    
+    for word, val in idfDict.items():
+        idfDict[word] = math.log(N / float(val))
+    return idfDict
+
+idfs = computeIDF([numOfWordsA, numOfWordsB])
+
+print(idfs)
